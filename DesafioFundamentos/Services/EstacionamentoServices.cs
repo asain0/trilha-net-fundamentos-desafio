a36385bc8342823a602cc9c02f3e9fc7c7cf8cee
anderson.araujo <anderson.araujo@auvo.com.br>
2022-11-15 10:17:16 -0300

    Remoção dos comentários e início da segmentação do código.

---
+++ DesafioFundamentos/Services/EstacionamentoServices.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioFundamentos.Services
+{
+    internal class EstacionamentoServices
+    {
+        public static bool IsVehicleOnParklot(string placa, List<string> veiculos)
+        {
+            return veiculos.Any(x => x.ToUpper() == placa.ToUpper());
+        }
+
+        internal static decimal TotalParkCost(decimal precoInicial, decimal precoPorHora)
+        {
+            Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+            
+            int horas = Int32.Parse(Console.ReadLine());
+            decimal valorTotal = precoInicial + (precoPorHora * horas);
+            return valorTotal;
+
+        }
+    }
+}
